17237b4e1bdaa6f9f76b1afd24a51f7df23acfcf
suyogsatyal <62869132+suyogsatyal@users.noreply.github.com>
2023-02-01 22:51:14 +0545

    Implemented Dark Mode(Styling Left)

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import DarkToggle from "./darkToggle";
 import "./dark.css";
 
 const Navbar = () => {
-    return(
+    const darkContains = document.querySelector('html').classList.contains('dark');
+    const [isChecked, setIsChecked] = useState(darkContains);
+
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    };
+
+    useEffect(() => {
+        document.querySelector('html').classList.toggle('dark', isChecked);
+    }, [isChecked]);
+
+    
+    return (
         <div className="relative">
             <div className="absolute lg:grid content-center -top-1 -right-1 h-16 w-500 dark:bg-[#2B2B2B] dark:text-gray-50 border border-gray-700 hidden rounded-bl-3xl">
                 <div className="flex flex-row justify-around px-3 text-gray-200">
@@ -11,7 +24,7 @@ const Navbar = () => {
                     <div className="cursor-pointer hover:text-gray-500 duration-75">Portfolio</div>
                     <div className="cursor-pointer hover:text-gray-500 duration-75">Blog</div>
                     <div className="cursor-pointer hover:text-gray-500 duration-75">Contact</div>
-                    <input id="toggle" className="toggle" type="checkbox"/>
+                    <input id="toggle" className="toggle" type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
                 </div>
             </div>
         </div>
--- src/assets/Sidebar.jsx
+++ src/assets/Sidebar.jsx
@@ -1,16 +1,28 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import downArrow from "./downArrow.svg";
-import DarkToggle from "./darkToggle";
+// import DarkToggle from "./darkToggle";
 import "./dark.css"
 import SidebarDetails from "./SidebarDetails";
 import SidebarNameCard from "./SidebarNameCard";
 
 const Sidebar = () => {
+    
     const [showElement, setShowElement] = useState(false);
 
-  const toggleContact = () => {
-    setShowElement(!showElement);
-  };
+    const toggleContact = () => {
+        setShowElement(!showElement);
+    };
+
+    const darkContains = document.querySelector('html').classList.contains('dark');
+    const [isChecked, setIsChecked] = useState(darkContains);
+
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    };
+
+    useEffect(() => {
+        document.querySelector('html').classList.toggle('dark', isChecked);
+    }, [isChecked]);
 
     return (
         <div className="sidebar sectionDarkBG basis-1/5 border border-gray-700 my-2 mx-0 sm:m-3 rounded-3xl overflow-hidden">
@@ -26,15 +38,15 @@ const Sidebar = () => {
                 <SidebarNameCard />
 
                 <div className="hidden xl:block">
-                    <SidebarDetails/>
+                    <SidebarDetails />
                 </div>
 
-                <div className={`${showElement? 'h-[346px] md:h-[218px] lg:h-[226px]': 'h-0'}  xl:hidden transition-all ease-in-out duration-300 overflow-hidden`}><SidebarDetails />
+                <div className={`${showElement ? 'h-[346px] md:h-[218px] lg:h-[226px]' : 'h-0'}  xl:hidden transition-all ease-in-out duration-300 overflow-hidden`}><SidebarDetails />
                 </div>
 
                 <div className="relative block xl:hidden rounded-3xl cursor-pointer">
                     <div className="darkToggle absolute block lg:hidden -right-3 sm:-right-6 -bottom-3 sm:-bottom-6 border border-gray-700 rounded-tl-3xl">
-                        <input id="toggle"  className="toggle m-2" type="checkbox" />
+                        <input id="toggle" className="toggle m-2 p-2" type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
                     </div>
                 </div>
             </div>
