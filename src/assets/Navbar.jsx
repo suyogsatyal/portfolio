023178a899ffb0e8f6cbc3f676093888fd98349d
Suyog Satyal <62869132+suyogsatyal@users.noreply.github.com>
2023-02-19 21:27:21 +0545

    Fixed light mode navbar hover

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -35,11 +35,11 @@ const Navbar = ({ setActiveTab }) => {
         <div className="relative">
             <div className="absolute lg:grid content-center -top-1 -right-1 h-16 w-500 dark:bg-[#2B2B2B] dark:text-gray-50 border border-gray-400 dark:border-gray-700 hidden rounded-bl-3xl">
                 <div className="flex flex-row justify-around px-3 text-gray-800 dark:text-gray-400">
-                    <div className={`cursor-pointer duration-75 ${activeTab === 1 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(1)}>About</div>
-                    <div className={`cursor-pointer duration-75 ${activeTab === 2 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(2)}>Resume</div>
-                    <div className={`cursor-pointer duration-75 ${activeTab === 3 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(3)}>Portfolio</div>
-                    <div className={`cursor-pointer duration-75 ${activeTab === 4 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(4)}>Blog</div>
-                    <div className={`cursor-pointer duration-75 ${activeTab === 5 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(5)}>Contact</div>
+                    <div className={`cursor-pointer duration-75 hover:text-gray-500 ${activeTab === 1 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(1)}>About</div>
+                    <div className={`cursor-pointer duration-75 hover:text-gray-500 ${activeTab === 2 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(2)}>Resume</div>
+                    <div className={`cursor-pointer duration-75 hover:text-gray-500 ${activeTab === 3 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(3)}>Portfolio</div>
+                    <div className={`cursor-pointer duration-75 hover:text-gray-500 ${activeTab === 4 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(4)}>Blog</div>
+                    <div className={`cursor-pointer duration-75 hover:text-gray-500 ${activeTab === 5 ? "dark:text-gray-100 dark:hover:text-gray-500" : "dark:hover:text-gray-500"}`} onClick={() => handleTabClick(5)}>Contact</div>
                     <input id="toggle" className="toggle" type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
                 </div>
             </div>
