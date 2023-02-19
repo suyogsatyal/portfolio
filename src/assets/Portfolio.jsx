e297a5ce80ed2bcef2e3231003181802e91e544f
Suyog Satyal <62869132+suyogsatyal@users.noreply.github.com>
2023-02-19 21:18:45 +0545

    Fixed to dark mode

--- src/assets/Portfolio.jsx
+++ src/assets/Portfolio.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const Portfolio = () =>{
     return(
-        <div className="">
-            <h2>
+        <div className="portfolioContent text-gray-600 dark:text-gray-200 whitespace-normal leading-relaxed">
+            <h2 className="productSansBold text-xl sm:text-[25px] text-gray-700 dark:text-gray-50 py-2">
                 Portfolio
             </h2>
         </div>
--- src/assets/Resume.jsx
+++ src/assets/Resume.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const Resume = () =>{
     return(
-        <div className="">
-            <h2>
+        <div className="text-gray-600 dark:text-gray-200 whitespace-normal leading-relaxed">
+            <h2 className="productSansBold text-xl sm:text-[25px] text-gray-700 dark:text-gray-50 py-2">
                 Resume
             </h2>
         </div>
